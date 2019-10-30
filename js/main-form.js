ddea56a18436e79c5b03ca9e33ba3be2745400c7
jahir2442 <jahir2442@gmail.com>
2019-10-30 07:37:57 -0500

    update Cambios a PHP

--- js/main-form.js
+++ js/main-form.js
@@ -1,6 +1,13 @@
+//darle una revision
+
 (function($) {
 
+
   $('#opcion').parent().append('<ul class="list-item" id="newopcion" name="opcion" ></ul>');
+
+
+  $('#opcion').parent().append('<ul class="list-item" id="newopcion" name="opcion" ></ul>');
+
   $('#opcion option').each(function(){
       $('#newopcion').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
   });
@@ -18,6 +25,7 @@
       $("#opcion").children('.init').html($(this).html());
       allOptions.toggle();
   });
+
 
   var marginSlider = document.getElementById('slider-margin');
   if (marginSlider != undefined) {
@@ -57,6 +65,9 @@
             email : true
         },
         phone_number : {
+            required: true,
+        },
+        ID: {
             required: true,
         }
     },
@@ -139,6 +150,9 @@
           },
           phone_number : {
               required: true,
+          },
+          ID: {
+              required: true,
           }
       },
       onfocusout: function(element) {
@@ -219,6 +233,93 @@
               email : true
           },
           phone_number : {
+              required: true,
+          },
+          ID: {
+              required: true,
+          }
+      },
+      onfocusout: function(element) {
+          $(element).valid();
+      },
+  });
+  
+      jQuery.extend(jQuery.validator.messages, {
+          required: "",
+          remote: "",
+          email: "",
+          url: "",
+          date: "",
+          dateISO: "",
+          number: "",
+          digits: "",
+          creditcard: "",
+          equalTo: ""
+      });
+  })(jQuery);
+
+  (function($) {
+
+    $('#opcion3').parent().append('<ul class="list-item" id="newopcion3" name= "opcion3"></ul>');
+    $('#opcion3 option').each(function(){
+        $('#newopcion3').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
+    });
+    $('#opcion3').remove();
+    $('#newopcion3').attr('id', 'opcion3');
+    $('#opcion3 li').first().addClass('init');
+    $("#opcion3").on("click", ".init", function() {
+        $(this).closest("#opcion2").children('li:not(.init)').toggle();
+    });
+    
+    var allOptions = $("#opcion3").children('li:not(.init)');
+    $("#opcion3").on("click", "li:not(.init)", function() {
+        allOptions.removeClass('selected');
+        $(this).addClass('selected');
+        $("#opcion3").children('.init').html($(this).html());
+        allOptions.toggle();
+    });
+  
+    var marginSlider = document.getElementById('slider-margin');
+    if (marginSlider != undefined) {
+        noUiSlider.create(marginSlider, {
+              start: [500],
+              step: 10,
+              connect: [true, false],
+              tooltips: [true],
+              range: {
+                  'min': 0,
+                  'max': 1000
+              },
+              format: wNumb({
+                  decimals: 0,
+                  thousand: ',',
+                  prefix: '$ ',
+              })
+      });
+    }
+    $('#reset').on('click', function(){
+        $('#register-form').reset();
+    });
+  
+    $('#register-form').validate({
+      rules : {
+          first_name : {
+              required: true,
+          },
+          last_name : {
+              required: true,
+          },
+          company : {
+              required: true
+          },
+          email : {
+              required: true,
+              email : true
+          },
+          phone_number : {
+              required: true,
+          },
+          ID: {
               required: true,
           }
       },
