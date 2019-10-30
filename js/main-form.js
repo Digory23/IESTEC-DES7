ab6c5045ccb2770a077fb287bdf2470ee3a00de9
LordDigory <54564313+LordDigory@users.noreply.github.com>
2019-10-30 08:34:43 -0500

    Simplificacion del main form js

--- js/main-form.js
+++ js/main-form.js
@@ -25,67 +25,6 @@
 
   
 
-  var marginSlider = document.getElementById('slider-margin');
-  if (marginSlider != undefined) {
-      noUiSlider.create(marginSlider, {
-            start: [500],
-            step: 10,
-            connect: [true, false],
-            tooltips: [true],
-            range: {
-                'min': 0,
-                'max': 1000
-            },
-            format: wNumb({
-                decimals: 0,
-                thousand: ',',
-                prefix: '$ ',
-            })
-    });
-  }
-  $('#reset').on('click', function(){
-      $('#register-form').reset();
-  });
-
-  $('#register-form').validate({
-    rules : {
-        first_name : {
-            required: true,
-        },
-        last_name : {
-            required: true,
-        },
-        company : {
-            required: true
-        },
-        email : {
-            required: true,
-            email : true
-        },
-        phone_number : {
-            required: true,
-        },
-        ID: {
-            required: true,
-        }
-    },
-    onfocusout: function(element) {
-        $(element).valid();
-    },
-});
-
-    jQuery.extend(jQuery.validator.messages, {
-        required: "",
-        remote: "",
-        email: "",
-        url: "",
-        date: "",
-        dateISO: "",
-        number: "",
-        digits: "",
-        creditcard: "",
-        equalTo: ""
-    });
 })(jQuery);
 
 (function($) {
@@ -109,68 +48,9 @@
         allOptions.toggle();
     });
   
-    var marginSlider = document.getElementById('slider-margin');
-    if (marginSlider != undefined) {
-        noUiSlider.create(marginSlider, {
-              start: [500],
-              step: 10,
-              connect: [true, false],
-              tooltips: [true],
-              range: {
-                  'min': 0,
-                  'max': 1000
-              },
-              format: wNumb({
-                  decimals: 0,
-                  thousand: ',',
-                  prefix: '$ ',
-              })
-      });
-    }
-    $('#reset').on('click', function(){
-        $('#register-form').reset();
-    });
-  
-    $('#register-form').validate({
-      rules : {
-          first_name : {
-              required: true,
-          },
-          last_name : {
-              required: true,
-          },
-          company : {
-              required: true
-          },
-          email : {
-              required: true,
-              email : true
-          },
-          phone_number : {
-              required: true,
-          },
-          ID: {
-              required: true,
-          }
-      },
-      onfocusout: function(element) {
-          $(element).valid();
-      },
-  });
-  
-      jQuery.extend(jQuery.validator.messages, {
-          required: "",
-          remote: "",
-          email: "",
-          url: "",
-          date: "",
-          dateISO: "",
-          number: "",
-          digits: "",
-          creditcard: "",
-          equalTo: ""
-      });
   })(jQuery);
+
+
 
   (function($) {
 
@@ -193,68 +73,8 @@
         allOptions.toggle();
     });
   
-    var marginSlider = document.getElementById('slider-margin');
-    if (marginSlider != undefined) {
-        noUiSlider.create(marginSlider, {
-              start: [500],
-              step: 10,
-              connect: [true, false],
-              tooltips: [true],
-              range: {
-                  'min': 0,
-                  'max': 1000
-              },
-              format: wNumb({
-                  decimals: 0,
-                  thousand: ',',
-                  prefix: '$ ',
-              })
-      });
-    }
-    $('#reset').on('click', function(){
-        $('#register-form').reset();
-    });
-  
-    $('#register-form').validate({
-      rules : {
-          first_name : {
-              required: true,
-          },
-          last_name : {
-              required: true,
-          },
-          company : {
-              required: true
-          },
-          email : {
-              required: true,
-              email : true
-          },
-          phone_number : {
-              required: true,
-          },
-          ID: {
-              required: true,
-          }
-      },
-      onfocusout: function(element) {
-          $(element).valid();
-      },
-  });
-  
-      jQuery.extend(jQuery.validator.messages, {
-          required: "",
-          remote: "",
-          email: "",
-          url: "",
-          date: "",
-          dateISO: "",
-          number: "",
-          digits: "",
-          creditcard: "",
-          equalTo: ""
-      });
   })(jQuery);
+
 
   (function($) {
 
@@ -277,6 +97,12 @@
         allOptions.toggle();
     });
   
+  })(jQuery);
+
+  
+
+(function($) {
+
     var marginSlider = document.getElementById('slider-margin');
     if (marginSlider != undefined) {
         noUiSlider.create(marginSlider, {
