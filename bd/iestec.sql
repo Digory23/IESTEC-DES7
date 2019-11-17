9ce8b75ab2717080cc4d8fead7c3f2f2cf5b73a2
LordDigory <54564313+LordDigory@users.noreply.github.com>
2019-11-17 00:42:57 -0500

    conexion e insercion bd

---
+++ bd/iestec.sql
@@ -0,0 +1,336 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-11-2019 a las 20:33:02
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `iestec2.0`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `ID_Cedula` varchar(15) NOT NULL,
+  `Pass` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `area_interes`
+--
+
+CREATE TABLE `area_interes` (
+  `ID_CodArea` int(15) NOT NULL,
+  `Descripcion` varchar(50) DEFAULT NULL,
+  `ID_Eventos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulos`
+--
+
+CREATE TABLE `articulos` (
+  `ID_Cedula` varchar(15) NOT NULL,
+  `Tipo_Part` varchar(25) DEFAULT NULL,
+  `Cod_Articulo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `ID_Eventos` int(11) NOT NULL,
+  `Nombre_Evento` varchar(30) NOT NULL,
+  `Hora` varchar(15) NOT NULL,
+  `Area` varchar(20) NOT NULL,
+  `ID_Sala` int(10) NOT NULL,
+  `Expositor` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `participantes`
+--
+
+CREATE TABLE `participantes` (
+  `ID_Cedula` varchar(15) NOT NULL,
+  `Cena` varchar(10) DEFAULT NULL,
+  `Institucion` varchar(20) DEFAULT NULL,
+  `Unidad` varchar(25) DEFAULT NULL,
+  `Pais` varchar(20) DEFAULT NULL,
+  `Ciudad` varchar(25) DEFAULT NULL,
+  `Provincia` varchar(30) DEFAULT NULL,
+  `Miembro_IEEE` varchar(15) DEFAULT NULL,
+  `Tipo_Participante` varchar(30) DEFAULT NULL,
+  `Area_Interes` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `participante_articulo`
+--
+
+CREATE TABLE `participante_articulo` (
+  `ID_Cedula` varchar(15) NOT NULL,
+  `ID_CedulaArt` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `participante_interes`
+--
+
+CREATE TABLE `participante_interes` (
+  `ID_Cedula` varchar(15) NOT NULL,
+  `Cod_Area` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programas`
+--
+
+CREATE TABLE `programas` (
+  `ID_Evento` int(11) NOT NULL,
+  `ID_Sala` int(10) NOT NULL,
+  `Fecha` date NOT NULL,
+  `ID_Usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sala`
+--
+
+CREATE TABLE `sala` (
+  `ID_Sala` int(10) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Descripcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_part`
+--
+
+CREATE TABLE `tipo_part` (
+  `ID_TipoPart` varchar(30) NOT NULL,
+  `Descrip` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `ID_TipoUsuario` varchar(30) NOT NULL,
+  `Descrip` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID_Usuario` int(11) NOT NULL,
+  `Nombre` varchar(20) DEFAULT NULL,
+  `Apellido` varchar(20) DEFAULT NULL,
+  `Sexo` char(1) DEFAULT NULL,
+  `Email` varchar(20) DEFAULT NULL,
+  `Telefono` varchar(15) DEFAULT NULL,
+  `Tipo_Ussuario` varchar(30) DEFAULT NULL,
+  `Cedula` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`ID_Cedula`);
+
+--
+-- Indices de la tabla `area_interes`
+--
+ALTER TABLE `area_interes`
+  ADD PRIMARY KEY (`ID_CodArea`),
+  ADD KEY `ID_Eventos` (`ID_Eventos`);
+
+--
+-- Indices de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  ADD PRIMARY KEY (`ID_Cedula`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`ID_Eventos`),
+  ADD KEY `ID_Sala` (`ID_Sala`);
+
+--
+-- Indices de la tabla `participantes`
+--
+ALTER TABLE `participantes`
+  ADD PRIMARY KEY (`ID_Cedula`),
+  ADD KEY `Tipo_Participante` (`Tipo_Participante`);
+
+--
+-- Indices de la tabla `participante_articulo`
+--
+ALTER TABLE `participante_articulo`
+  ADD UNIQUE KEY `ID_Cedula` (`ID_Cedula`,`ID_CedulaArt`),
+  ADD KEY `fk_Articulo` (`ID_CedulaArt`);
+
+--
+-- Indices de la tabla `participante_interes`
+--
+ALTER TABLE `participante_interes`
+  ADD PRIMARY KEY (`ID_Cedula`),
+  ADD KEY `fk_Area` (`Cod_Area`);
+
+--
+-- Indices de la tabla `programas`
+--
+ALTER TABLE `programas`
+  ADD PRIMARY KEY (`ID_Evento`),
+  ADD KEY `ID_Sala` (`ID_Sala`),
+  ADD KEY `ID_Usuario` (`ID_Usuario`);
+
+--
+-- Indices de la tabla `sala`
+--
+ALTER TABLE `sala`
+  ADD PRIMARY KEY (`ID_Sala`);
+
+--
+-- Indices de la tabla `tipo_part`
+--
+ALTER TABLE `tipo_part`
+  ADD PRIMARY KEY (`ID_TipoPart`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`ID_TipoUsuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID_Usuario`),
+  ADD UNIQUE KEY `Cedula` (`Cedula`),
+  ADD KEY `Tipo_Ussuario` (`Tipo_Ussuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `area_interes`
+--
+ALTER TABLE `area_interes`
+  MODIFY `ID_CodArea` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `ID_Eventos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID_Usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD CONSTRAINT `fk_eventos` FOREIGN KEY (`ID_Eventos`) REFERENCES `area_interes` (`ID_Eventos`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `participantes`
+--
+ALTER TABLE `participantes`
+  ADD CONSTRAINT `fk_Articulos` FOREIGN KEY (`ID_Cedula`) REFERENCES `participante_articulo` (`ID_Cedula`),
+  ADD CONSTRAINT `fk_ParticipanteInteres` FOREIGN KEY (`ID_Cedula`) REFERENCES `participante_interes` (`ID_Cedula`),
+  ADD CONSTRAINT `fk_TipoPart` FOREIGN KEY (`Tipo_Participante`) REFERENCES `tipo_part` (`ID_TipoPart`);
+
+--
+-- Filtros para la tabla `participante_articulo`
+--
+ALTER TABLE `participante_articulo`
+  ADD CONSTRAINT `fk_Articulo` FOREIGN KEY (`ID_CedulaArt`) REFERENCES `articulos` (`ID_Cedula`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `participante_interes`
+--
+ALTER TABLE `participante_interes`
+  ADD CONSTRAINT `fk_Area` FOREIGN KEY (`Cod_Area`) REFERENCES `area_interes` (`ID_CodArea`);
+
+--
+-- Filtros para la tabla `programas`
+--
+ALTER TABLE `programas`
+  ADD CONSTRAINT `fk_Evento` FOREIGN KEY (`ID_Evento`) REFERENCES `eventos` (`ID_Eventos`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_Sala` FOREIGN KEY (`ID_Sala`) REFERENCES `sala` (`ID_Sala`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_Partic` FOREIGN KEY (`Cedula`) REFERENCES `participantes` (`ID_Cedula`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_Programa` FOREIGN KEY (`ID_Usuario`) REFERENCES `programas` (`ID_Usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_admin` FOREIGN KEY (`Cedula`) REFERENCES `administrador` (`ID_Cedula`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_tipousu` FOREIGN KEY (`Tipo_Ussuario`) REFERENCES `tipo_usuario` (`ID_TipoUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ inserts/insertar_EPregrado.php
@@ -0,0 +1,28 @@
+<?php 
+include "conexion.php";
+
+
+//variables del metodo POST
+$nombre = $_POST['first_name'];
+$apellido = $_POST['last_name'];
+$cedula = $_POST['ID'];
+//$sexo = $_POST['opcion'];
+$email = $_POST['email'];
+$telefono = $_POST['phone_number'];
+
+//insercion en la tabla
+$sql = "INSERT INTO usuario (nombre, apellido, email, Cedula) VALUES (?, ?, ?, ?)";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute([$nombre, $apellido, $email, $cedula]);
+
+        if($stmt->rowCount() > 0)
+        {
+           
+            echo "Registro exitoso";
+            header('Location: ../index.php');
+        }
+        else{
+            echo "Error";
+            
+        }
+?>
