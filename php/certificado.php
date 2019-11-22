142ab111367a0ffcd8c7e4253ae1c259e672790f
jahir2442 <jahir2442@gmail.com>
2019-11-22 12:49:53 -0500

    certificado3.0

--- php/certificado.php
+++ php/certificado.php
@@ -1,19 +1,28 @@
 <?php
 require '../fpdf/fpdf.php';
 require '../inserts/conexion.php';
+//$cedula= $_GET['ID'];
+
+  $stmt2 = $dbh->prepare("SELECT Nombre, Apellido FROM usuario WHERE ID_Usuario = 7");
+  $stmt2->bindParam(':user', $username);
+      $stmt2->setFetchMode(PDO::FETCH_ASSOC);
+
+  
+      $stmt2->execute();
+      $row = $stmt2->fetch();
 
 
-
-$pdf= new FPDF();
+$pdf= new FPDF('L','mm','A4');
 $pdf->Addpage();
-
-$pdf->SetFont('Arial','B','20');
-$pdf->Image('../img/certificado.png',20,20,-300);
+//$pdf->Set_paper('letter','landscape');
+$pdf->SetFont('times','B','20');
+$pdf->Image('../img/certificado.png',15,10,-190);
 
 $pdf->SetXY(55,60);
-$pdf->Cell(100, 20, 'Nombre',20,20,'C');
+$pdf->Cell(140, 60, $row['Nombre'],20,0,'C');
+$pdf->Cell(-50, 60, $row['Apellido'],20,20,'C');
+
 
 $pdf->output();
-
 
 ?>
