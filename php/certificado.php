b2da27a678dbd0265c5c29e519b0994bbbe4a47c
jahir2442 <jahir2442@gmail.com>
2019-11-22 09:22:57 -0500

    certificado

--- php/certificado.php
+++ php/certificado.php
@@ -1,1 +1,18 @@
 <?php
+require '../fpdf/fpdf.php';
+require '../inserts/conexion.php';
+
+
+$pdf= new FPDF();
+$pdf->Addpage();
+
+$pdf->SetFont('Arial','B','20');
+$pdf->Image('../img/certificado.png',20,20,-300);
+
+$pdf->SetXY(55,60);
+$pdf->Cell(100, 20, 'Nombre',20,20,'C');
+
+$pdf->output();
+
+
+?>
