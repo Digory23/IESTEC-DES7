8778f01c63e2490a9deeba14433cf406eb613058
LordDigory <54564313+LordDigory@users.noreply.github.com>
2019-11-23 15:59:26 -0500

    cambio de pdf

--- php/certificado.php
+++ php/certificado.php
@@ -1,10 +1,10 @@
 <?php
 require '../fpdf/fpdf.php';
-require '../inserts/conexion.php';
-//$cedula= $_GET['ID'];
+require '../conexion/conexion.php';
+$cedula= $_GET['cedula'];
 
-  $stmt2 = $dbh->prepare("SELECT Nombre, Apellido FROM usuario WHERE ID_Usuario = 7");
-  $stmt2->bindParam(':user', $username);
+  $stmt2 = $dbh->prepare("SELECT Nombre, Apellido FROM usuario WHERE Cedula=:cedu");
+  $stmt2->bindParam(':cedu', $cedula);
       $stmt2->setFetchMode(PDO::FETCH_ASSOC);
 
   
@@ -14,7 +14,7 @@ require '../inserts/conexion.php';
 
 $pdf= new FPDF('L','mm','A4');
 $pdf->Addpage();
-//$pdf->Set_paper('letter','landscape');
+
 $pdf->SetFont('times','B','20');
 $pdf->Image('../img/certificado.png',15,10,-190);
 
@@ -22,7 +22,8 @@ $pdf->SetXY(55,60);
 $pdf->Cell(140, 60, $row['Nombre'],20,0,'C');
 $pdf->Cell(-50, 60, $row['Apellido'],20,20,'C');
 
-
+header('Location: RegistroExitoso.php');
 $pdf->output();
+
 
 ?>
