55d0a36eed574b94a08e619074c0decaaf840987
LordDigory <54564313+LordDigory@users.noreply.github.com>
2019-11-26 11:17:07 -0500

    correcciones

--- controllers/Qr.php
+++ controllers/Qr.php
@@ -44,5 +44,43 @@
 	
         //Enviamos los parametros a la Función para generar código QR 
     QRcode::png($contenido, $filename, $level, $tamaño, $framSize); 
-    header('Location: ../php/RegistroExitoso.php');
+    header("Location: certificado.php?cedula=$cedula");
+
+    //enviando correo con la función Mail
+/*
+$titulo    = 'CERTIFICADO DE PARTICIPACION';
+$mensaje   = 'GRACIAS POR PARTICIPAR';
+$filename = "$cedula.pdf";
+$ruta = "../certificados/";
+
+//configuracion de Header HTML
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$cabeceras .= 'From: congreso@iestec.local';
+
+//Mensaje en HTML
+ $mensajeContenido='<html>
+    <head>
+    </head>
+    <body>
+    <table width="100%"  border="0" cellpadding="2" cellspacing="2">
+      <tr>
+        <td><h3 align="center"><embed src="../certificados/'.$filename.'" type="application/pdf" width="100%" height="600px" /><br>
+          Congreso IESTEC<br>
+        </h3>
+        </td>
+      </tr>
+      <tr>
+        <td>'.$mensaje.'</td>
+      </tr>
+      <tr>
+        <td bgcolor="#660066">&nbsp;</td>
+      </tr>
+    </table>
+    </body>
+    </html>';
+
+	//Función mail
+	if (mail($para, $titulo, $mensajeContenido, $cabeceras))
+	echo "El mensaje se ha enviado correctamente";
 ?>
--- inserts/insertar_EN.php
+++ inserts/insertar_EN.php
@@ -30,7 +30,7 @@ $sql = "INSERT INTO usuario (Nombre, Apellido, Sexo, Email, Telefono, Miembro_IE
         {
             echo "Registro exitoso";
             //header('Location: ../php/RegistroExitoso.php');
-            header("Location: ../controllers/certificado.php?cedula=$cedula");
+            //header("Location: ../controllers/certificado.php?cedula=$cedula");
             header("Location: ../controllers/QR.php?cedula=$cedula");
         }
         else{
