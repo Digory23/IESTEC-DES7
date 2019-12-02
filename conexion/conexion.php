492ac2c5babd1accfc852cbcd04427969506a708
LordDigory <54564313+LordDigory@users.noreply.github.com>
2019-12-01 21:45:22 -0500

    ajustes conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,10 +1,16 @@
 <?php
 
-//Definicion de variables de conexion
+//Definicion de variables de conexion local
 define("bdNom", "iestec");
 define("bdUser", "root");
 define("bdPass", "");
 define("bdHost", "localhost");
+
+//conexion en el host
+/* define("bdNom", "congreso_utp1");
+define("bdUser", "congreso1");
+define("bdPass", "db.congreso_2019");
+define("bdHost", "mysql.congreso1.ds507.online"); */
 
 //Conexion a la base de datos
 try{
