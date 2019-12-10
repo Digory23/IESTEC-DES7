8b1cda10a62e1b9a90ee568b1b19dbc084f8385f
kujakux <55299075+kujakux@users.noreply.github.com>
2019-12-09 19:32:57 -0500

    Estadisticas y Tabla de Datos

--- php/Estadisticas.php
+++ php/Estadisticas.php
@@ -129,25 +129,15 @@ include "../controllers/validar.php";
                             <!-- <li><a href="./index-2.html">Home 2</a></li> -->
                         </ul>
                     </li>
-<<<<<<< HEAD
-                    <li class="nav-label">Table</li>
-                    <li>
-                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                            <i class="icon-menu menu-icon"></i><span class="nav-text">Table</span>
-=======
                     <li class="nav-label">Tabla</li>
                     <li>
                         <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                             <i class="icon-menu menu-icon"></i><span class="nav-text">Tabla</span>
->>>>>>> origin/Daniel_dev
                         </a>
                         <ul aria-expanded="false">
                             <li><a href="./table-datatable.php" aria-expanded="false">Tabla de Datos</a></li>
                         </ul>
                     </li>
-<<<<<<< HEAD
-                    
-=======
                     <li class="nav-label">Regresar</li>
                     <li>
                         <a class="has-arrow" href="javascript:void()" aria-expanded="false">
@@ -157,7 +147,6 @@ include "../controllers/validar.php";
                             <li><a href="../controllers/salir.php" aria-expanded="false">Regresar al inicio</a></li>
                         </ul>
                     </li>
->>>>>>> origin/Daniel_dev
                 </ul>
             </div>
         </div>
@@ -171,56 +160,6 @@ include "../controllers/validar.php";
         <div class="content-body">
 
             <div class="container-fluid mt-3">
-                <div class="row">
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-1">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Entradas Vendidas</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white">4565</h2>
-                                    <p class="text-white mb-0">Octubre 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-shopping-cart"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-2">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Ganancias</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white">$ 8541</h2>
-                                    <p class="text-white mb-0">Octubre 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-money"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-3">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Nuevos Clientes</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white">4565</h2>
-                                    <p class="text-white mb-0">Octubre 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-users"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-4">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Servicio al Cliente</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white">99%</h2>
-                                    <p class="text-white mb-0">Octubre 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-heart"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <div class="row">
                     <div class="col-lg-12">
