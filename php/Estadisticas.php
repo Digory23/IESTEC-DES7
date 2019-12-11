1243b50026a4af0556116f0fd618c150a9d89be2
nath <54672324+ndcaz12@users.noreply.github.com>
2019-12-11 02:10:13 -0500

    aa

--- php/Estadisticas.php
+++ php/Estadisticas.php
@@ -95,6 +95,19 @@ include "../controllers/estadisticas-controller.php"
                 </div>
 
                 <!--**********************************
+                <div class="nav-control">
+                    <div class="hamburger">
+                        <span class="toggle-icon"><i class="icon-menu"></i></span>
+                    </div>
+                </div>
+                <div class="header-left">
+                    <div class="input-group icons">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-transparent border-0 pr-2 pr-sm-3" id="basic-addon1">
+                        </div>
+                    </div>
+                </div>
+
             Nav header start
         ***********************************-->
         <div class="nav-header">
