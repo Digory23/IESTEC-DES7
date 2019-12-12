62e63a6b3305471b8003e668fa0beae4d064c38a
kujakux <55299075+kujakux@users.noreply.github.com>
2019-12-12 07:42:30 -0500

    cambios

--- php/EnvioExitoso.php
+++ php/EnvioExitoso.php
@@ -19,7 +19,7 @@
 	<div class="container-login100" style="background-image: url('../img/bg1.jpg');">
 		<div class="wrap-login101 p-l-55 p-r-55 p-t-80 p-b-30" >
 
-			<form action="../index.html" class="login100-form validate-form" method="POST" class="register-form" id="register-form">
+			<form action="../controllers/salir.php" class="login100-form validate-form" method="POST" class="register-form" id="register-form">
 				<span class="login100-form-title p-b-37">
                 <img class="img-responsive animation slideDown" src="../img/correct.png" alt="" width="150" height="150"><br>
 					¡Envió Exitoso!
